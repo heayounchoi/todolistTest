917264e4491ead8c3e23d7a78021ff27872170ec
heayounchoi <heayounchoi@gmail.com>
2022-12-01 12:00:29 +0900

    sdasd

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
+import React, {useState} from "react";
 import './App.css';
 
+const Todo = ({item, todo}) => {
+    return (
+        <div>
+            <span>{item.body}</span>
+        </div>
+    )
+}
+
 function App() {
+    const [input, setInput] = useState('')
+    const [todo, setTodo] = useState([])
+
+    const onClickAddButton = () => {
+        const newTodo = todo.concat({
+            id: todo.length,
+            body: input,
+        })
+        setTodo(newTodo)
+
+        setInput('')
+    }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input type="text" onChange={(e) => setInput(e.target.value)} value={input}/><button type="submit" onClick={onClickAddButton}>추가하기</button>
+        <h1>Todo List</h1>
+        <ul>
+            {todo &&
+                todo.map((item) => {
+                return <Todo item={item} todo={todo} key={todo.id} />
+            })}
+        </ul>
     </div>
   );
 }
